ddb555b04cb6ccea39be9445aca09d4393c85e25
zhongyangxun <zhongyangxun@163.com>
2020-06-14 20:17:33 +0800

    Add story of Button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,21 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: async config => {
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [['react-app', { flow: false, typescript: true }]],
+        },
+      }
+    );
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import Button from './button'
+
+const defaultButton = () => (
+  <Button onClick={action('clicked')} >default button</Button>
+)
+
+const buttonWithSize = () => (
+  <>
+    <Button size="lg">large button</Button>
+    <Button size="sm">small button</Button>
+  </>
+)
+
+const buttonWithType = () => (
+  <>
+    <Button btnType="primary">primary button</Button>
+    <Button btnType="danger">danger button</Button>
+    <Button btnType="link" href="https://google.com" >link button</Button>
+  </>
+)
+
+storiesOf('Button Component', module)
+  .add('默认 Button', defaultButton)
+  .add('不同尺寸的 Button', buttonWithSize)
+  .add('不同类型的 Button', buttonWithType)
