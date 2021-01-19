5e6ca4df297b10f6be4fdae3e24ab42a7f857d96
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-19 14:42:47 +0000

    Deploy zhongyangxun/knight to github.com/zhongyangxun/knight.git:gh-pages

---
+++ precache-manifest.926852e9d8080a7ce7460b6ea95e3104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c0564f384ea2f7b10fe4f8a87231ee",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e6ccbb59ef302bfb5405.bundle.js"
+  },
+  {
+    "url": "runtime~main.e6ccbb59ef302bfb5405.bundle.js"
+  },
+  {
+    "revision": "f11e83cbf8481ced0fcf",
+    "url": "static/css/main.3e347c29.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.e6ccbb59ef302bfb5405.bundle.js"
+  },
+  {
+    "url": "vendors~main.e6ccbb59ef302bfb5405.bundle.js.LICENSE.txt"
+  }
+]);
